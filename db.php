e2643c17be14c60dd1e03f5587532b534910467f
NateGithub9 <nathaniel.akingbade@gmail.com>
2024-07-26 16:31:50 +0200

    ajout gitignore, composer, php.env, modif de db.php

--- db.php
+++ db.php
@@ -1,15 +1,18 @@
 <?php
-$servername = "localhost";  
-$username = "root";         
-$password = "resto2.0"; 
-$dbname = "resto2";     
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];  
+$username = $_ENV['DB_USER'];         
+$password = $_ENV['DB_PASS']; 
+$dbname = $_ENV['DB_NAME'];     
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit();
 }
-?>
